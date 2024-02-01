6481997b027ae04a32f20804ca62e3a7082b312a
Vitaliy <131911773+whyzzz20@users.noreply.github.com>
2024-02-01 18:13:14 +0100

    recreate first task

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,86 +1,84 @@
-//Імпорти бібліотек
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-//Посилання на елементи сторінки
-const btnRef = document.querySelector('button[data-start]');
-const daysRef = document.querySelector('span[data-days]');
-const hoursRef = document.querySelector('span[data-hours]');
-const minutesRef = document.querySelector('span[data-minutes]');
-const secondsRef = document.querySelector('span[data-seconds]');
-const datePickerRef = document.querySelector('#datetime-picker');
-btnRef.disabled = true;
+
+const myInput = document.querySelector("#datetime-picker");
+
+const startBtn = document.querySelector('[data-start]');
+const timerDays = document.querySelector('[data-days]');
+const timerHours = document.querySelector('[data-hours]');
+const timerMinutes = document.querySelector('[data-minutes]');
+const timerSeconds = document.querySelector('[data-seconds]');
 
 let userSelectedDate;
 
-//Налаштування календаря
-const flatpickrOptions = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    if (selectedDates[0] - Date.now() < 0) {
-      // window.alert('Please choose a date in the future');
-      iziToast.error({
-        title: 'Wrong Date',
-        message: 'Please choose a date in the future',
-      });
-      btnRef.disabled = true;
+startBtn.disabled = true;
 
-      return;
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+    onClose(selectedDates){
+      userSelectedDate = selectedDates[0]
+      if(selectedDates[0] <= Date.now()){
+        iziToast.show({
+          title: '❌',
+          message: 'Please choose a date in the future!',
+          position: 'topRight',
+          messageColor: '#fff',
+          messageSize: '20px',
+          backgroundColor: '#f54245',
+          close: false,
+          closeOnClick: true,
+          progressBarEasing: 'linear',
+        })
+      }else{
+        startBtn.disabled = false;
+        
+      }
+
     }
+  };
+  flatpickr(myInput , options);
 
-    userSelectedDate = selectedDates[0];
-    btnRef.disabled = false;
-  },
-};
-//Встановлення календаря
-flatpickr('#datetime-picker', flatpickrOptions);
+  startBtn.addEventListener('click', e => {
+    const timer = setInterval(() => {
+      const result = userSelectedDate - Date.now();
+      const timeValue = convertMs(result);
+      if (result <= 0) {
+        clearInterval(timer);
+      } else {
+        timerDays.textContent = addLeadingZero(timeValue.days);
+        timerHours.textContent = addLeadingZero(timeValue.hours);
+        timerMinutes.textContent = addLeadingZero(timeValue.minutes);
+        timerSeconds.textContent = addLeadingZero(timeValue.seconds);
+      }
+    }, 1000);
+  });
 
-//Запуск таймера
+  function addLeadingZero(value) {
+    value = String(value);
+    return value.length < 2 ? value.padStart(2, '0') : value;
+  }
 
-btnRef.addEventListener('click', startTimer);
+  function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
-function startTimer(e) {
-  const timerInterval = setInterval(() => {
-    const { days, hours, minutes, seconds } = convertMs(
-      userSelectedDate - Date.now()
-    );
-    btnRef.disabled = true;
-    datePickerRef.disabled = true;
+    const days = Math.floor(ms / day);
 
-    if (seconds <= 0) {
-      clearInterval(timerInterval);
-      btnRef.disabled = false;
-      datePickerRef.disabled = false;
-    }
+    const hours = Math.floor((ms % day) / hour);
 
-    daysRef.textContent = days.toString().padStart(2, 0);
-    hoursRef.textContent = hours.toString().padStart(2, 0);
-    minutesRef.textContent = minutes.toString().padStart(2, 0);
-    secondsRef.textContent = seconds.toString().padStart(2, 0);
-  }, 1000);
-}
-
-//Конвертер часу
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
